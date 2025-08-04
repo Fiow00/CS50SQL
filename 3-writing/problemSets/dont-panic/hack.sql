dc63bc212628d898b57ba1d90ad72022f937c0a3
fiow <ahmedyhya5325500@gmail.com>
2025-08-04 10:00:56 +0300

    Lecture 3

---
+++ 3-writing/problemSets/dont-panic/hack.sql
@@ -0,0 +1,8 @@
+UPDATE "users" SET "password" = '982c0381c279d139fd221fce974916e7'
+WHERE "username" = 'admin';
+
+DELETE FROM "user_logs" WHERE "id" = (SELECT "id" FROM "user_logs" WHERE "old_username" = 'admin');
+
+INSERT INTO "user_logs" ("type", "old_username", "new_username", "new_password")
+VALUES
+('update', 'admin', 'admin', (SELECT "password" FROM "users" WHERE "username" = 'emily33'));
---
+++ 3-writing/problemSets/meteorites/import.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "meteorites_temp" (
+    "name" TEXT,
+    "id" INTEGER,
+    "nametype" TEXT,
+    "class" TEXT,
+    "mass" REAL,
+    "discovery" TEXT CHECK("discovery" IN ('Fell', 'Found')),
+    "year" REAL,
+    "lat" REAL,
+    "long" REAL,
+    PRIMARY KEY("id")
+);
+
+.import --csv --skip 1 meteorites.csv meteorites_temp
+
+UPDATE "meteorites_temp" SET "mass" = NULL WHERE "mass" = '';
+UPDATE "meteorites_temp" SET "year" = NULL WHERE "year" = '';
+UPDATE "meteorites_temp" SET "lat" = NULL WHERE "lat" = '';
+UPDATE "meteorites_temp" SET "long" = NULL WHERE "long" = '';
+
+UPDATE "meteorites_temp"
+SET "mass" = ROUND("mass", 2),
+    "lat" = ROUND("lat", 2),
+    "long" = ROUND("long", 2);
+
+CREATE TABLE "meteorites" (
+    "name" TEXT,
+    "id" INTEGER,
+    "class" TEXT,
+    "mass" REAL,
+    "discovery" TEXT CHECK("discovery" IN ('Fell', 'Found')),
+    "year" REAL,
+    "lat" REAL,
+    "long" REAL,
+    PRIMARY KEY("id")
+);
+
+INSERT INTO "meteorites" ("name", "class", "mass", "discovery", "year", "lat", "long")
+SELECT "name", "class", "mass", "discovery", "year", "lat", "long" FROM "meteorites_temp"
+WHERE "nametype" != 'Relict'
+ORDER BY "year", "name";
